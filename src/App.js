6a3a66bed2c2d7c5b3dd5bd1ac0e7ec565526193
Takuma Nitta <takuma@TakumanoMacBook-Pro.local>
2021-08-11 03:03:22 +0900

    Update station3

--- src/App.js
+++ src/App.js
@@ -9,9 +9,16 @@ import './App.css'
  */
 export const App = () => {
   return (
-    <header>
-      Dogアプリ
-    </header>
-    
+    <div>
+      <header>
+        <h1>Dogアプリ</h1>
+      </header>
+      <main>
+        <div>
+          <p>犬の画像を表示するサイトです</p>
+          <img src="https://images.dog.ceo/breeds/spaniel-brittany/n02101388_2667.jpg" />
+        </div>
+      </main>
+    </div>
   )
 }
