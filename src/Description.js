f0a73ae42315216207c6a0581906db307b5e3f3d
Takuma Nitta <s1922025@stu.musashino-u.ac.jp>
2021-11-22 16:44:48 +0900

    station8

--- src/Description.js
+++ src/Description.js
@@ -1,6 +1,6 @@
 // DO NOT DELETE
 import * as React from 'react'
-import { Dogimage } from './DogImage.js';
+import { DogImage } from './DogImage.js';
 
 export function Description(props) {
     return (
@@ -10,7 +10,7 @@ export function Description(props) {
                     <p>{props.desc}</p>
                 </div>
                 <div className="rightColumn">
-                    <Dogimage url="https://images.dog.ceo/breeds/spaniel-brittany/n02101388_2667.jpg" />
+                    <DogImage url="https://images.dog.ceo/breeds/spaniel-brittany/n02101388_2667.jpg" />
                 </div>
             </div>
         </main>
